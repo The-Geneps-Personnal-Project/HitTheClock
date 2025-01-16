b296cc330dcb301a19f46d5fa6840fb3cf9f5b6f
Julien Delphine <julien.delphine@epitech.eu>
2025-01-16 16:10:22 +0100

    feat(homepage): add read the rules button

--- app/Home/HomePage.styles.ts
+++ app/Home/HomePage.styles.ts
@@ -1,16 +1,27 @@
 import { StyleSheet } from "react-native";
 
 import SpotifyButtonStyles from "../../components/Buttons/SpotifyButton.styles";
+import RulesButtonStyles from "@/components/Buttons/RulesButton.styles";
 
 export default StyleSheet.create({
   ...SpotifyButtonStyles,
+  ...RulesButtonStyles,
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "#0B1616", // Dorian black
+  },
+  buttonContainer: {
+    marginTop: 150,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   title: {
+    paddingTop: 150,
     fontSize: 24,
     fontWeight: "bold",
+    color: "#fff",
   },
 });
---
+++ components/Buttons/RulesButton.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  rulesButtonContainer: {
+    padding: 15,
+    flexDirection: "row",
+    height: 80,
+    width: 250,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  rulesButtonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
